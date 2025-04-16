4d5c14edb50b66cb21c1263b3af88aa1749abf92
ElBenjas4000 <sbmartingonzalez@escuelasproa.edu.ar>
2025-04-16 11:10:44 -0300

    Update benjaMartinAbrilDos.py

--- benjaMartinAbrilDos.py
+++ benjaMartinAbrilDos.py
@@ -5,10 +5,63 @@ def clear():
 
 def CantidadTareas(cant_de_tareas):
     diccionario_de_tareas = {}
+
     for i in range(len(cant_de_tareas)):
-        diccionario_de_tareas.update(f"{i+1}. ", cant_de_tareas[i])
-    return diccionario_de_tareas
+        diccionario_de_tareas[i+1] = cant_de_tareas[i]
 
-cantidad_de_tareas = ["Acomodar la cama", "Cocinar", "Lavar la ropa"]
+    for nuemro, tarea in diccionario_de_tareas.items():
+        print(f"{nuemro}. ", tarea)
 
-CantidadTareas(cantidad_de_tareas)
+cantidad_de_tareas = []
+
+while True:
+    clear()
+    print("Gestion de tareas")
+    print()
+    print("1. Añadir una terea")
+    print(f"2. Ver todas las Tareas (en total son {len(cantidad_de_tareas)})")
+    print("3. Marcar una tarea como completa")
+    print("4. Salir")
+
+    while True:
+        try:
+            accion = int(input("Ingrese el numero de lo que quiere hacer:  "))
+            break
+        except:
+            print("Tiene que ingresar un numero")
+
+    
+    if accion == 1:
+        print("Que tarea desea agregar")
+        tarea = str(input(">> "))
+        cantidad_de_tareas.append(tarea)
+        continue
+
+    elif accion == 2:
+        clear()
+        CantidadTareas(cantidad_de_tareas)
+
+        while True:
+            try:
+                volver = int(input("Ingrese 1 para volver:  "))
+                break
+            except:
+                print("Tiene que ingresar un numero")
+
+    elif accion == 3:
+        clear()
+        CantidadTareas(cantidad_de_tareas)
+        print("¿Qué numero de tarea quiere borrar?")
+        tarea_para_eliminar = int(input(">> "))
+        cantidad_de_tareas.remove(tarea_para_eliminar - 1)
+        while True:
+            try:
+                volver = int(input("Ingrese 1 para volver:  "))
+                break
+            except:
+                print("Tiene que ingresar un numero")
+
+    elif accion == 4:
+        break
+
+print("Espero que cumplas tus tareas. Nos vemos")
